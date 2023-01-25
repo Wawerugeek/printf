a03eb070b1fff503dc2b03ce23163bdb0eab5bd2
Wawerugeek <stevemuiruri84@gmail.com>
2023-01-25 10:48:13 +0000

    update on function that produces output

--- _printf.c
+++ _printf.c
@@ -51,3 +51,17 @@ int _printf(const char *format, ...)
 
 	return (printed_chars);
 }
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
+
