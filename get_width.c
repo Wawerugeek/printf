395aacb11dcb49f0ab133d690ec0b16fa5fdfa20
Addiowen <owenoscar18@gmail.com>
2023-01-25 15:30:19 +0300

    get_witdh

--- get_width.c
+++ get_width.c
@@ -16,4 +16,21 @@ int get_width(const char *format, int *i, va_list list)
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
 		if (is_digit(format[curr_i]))
+		{
+			width *= 10;
+			width += format[curr_i] - '0';
 		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = curr_i - 1;
+
+	return (width);
+}
