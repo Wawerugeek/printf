a94eff595e256ceebe7f123a4178ef3a1cc2a51a
Addiowen <owenoscar18@gmail.com>
2023-01-25 15:20:49 +0300

    precision

--- get_precision.c
+++ get_precision.c
@@ -18,4 +18,24 @@ int get_precision(const char *format, int *i, va_list list)
 
 	precision = 0;
 
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
 
+	*i = curr_i - 1;
+
+	return (precision);
+}
