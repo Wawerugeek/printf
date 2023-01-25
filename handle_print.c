4bcde301f17f82cf927b27717c2ca01bfd417549
Wawerugeek <stevemuiruri84@gmail.com>
2023-01-25 12:36:32 +0000

    more updates

--- handle_print.c
+++ handle_print.c
@@ -1,3 +1,17 @@
+#include "main.h"
+/**
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: 1 or 2;
+ */
+
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -24,3 +38,13 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{
 			--(*ind);
 			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+				--(*ind);
+			if (fmt[*ind] == ' ')
+				--(*ind);
+			return (1);
+		}
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
+	}
+	return (printed_chars);
+}
